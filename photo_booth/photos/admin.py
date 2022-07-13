1da70487dcca0e765ddbba6bf449799f96eb4885
av-kutergin <av.kutergin@gmail.com>
2022-07-13 20:28:08 +0400

    Added HTML templates and models. Modified views.

--- photo_booth/photos/admin.py
+++ photo_booth/photos/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Photo
+from .models import Photo, Operators, JournalPhoto, Journals, Cities
 
 admin.site.register(Photo)
+admin.site.register(Operators)
+admin.site.register(JournalPhoto)
+admin.site.register(Journals)
+admin.site.register(Cities)
+
