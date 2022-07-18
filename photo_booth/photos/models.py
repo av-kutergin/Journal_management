8cfb0d52e23d29488488cb609462495c9763e7d9
av-kutergin <av.kutergin@gmail.com>
2022-07-18 19:02:50 +0400

    Added view logic, altered templates, minor changes in models.

--- photo_booth/photos/models.py
+++ photo_booth/photos/models.py
@@ -1,37 +1,41 @@
 from django.contrib.auth.models import User
-from django.contrib import admin
 from django.db import models
+from django.urls import reverse
 
 
-class Photo(models.Model):
-    photo_name = models.CharField(max_length=255, default=None)
-    description = models.CharField(max_length=255)
-    display = models.BooleanField(default=True)
-    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, default=1)
-
-
-class Operators(User):
+class User(User):
     pass
-
-    # allowed_cities = models.
-
 
 
 class Cities(models.Model):
-    city_name = models.CharField(max_length=20, db_index=True, )
+    city_name = models.CharField(max_length=20, db_index=True)
+    operators = models.ManyToManyField(User)
 
     def __str__(self):
         return self.city_name
 
+    def get_absolute_url(self):
+        # return reverse('journal', kwargs={'name': self.city_name})
+        return reverse('journal', kwargs={'city_id': self.pk})
+
 
 class Journals(models.Model):
     journal_city = models.ForeignKey('Cities', on_delete=models.CASCADE, null=False)
-    journal_owner = models.ForeignKey('Operators', on_delete=models.CASCADE, null=False)
-    journal_number = models.IntegerField(null=False)
-    # journal_filling = models.
+    journal_owner = models.ForeignKey('User', on_delete=models.CASCADE, null=False)
+    time_create = models.DateTimeField(auto_now_add=True)
+
+    def show_info(self, city_id):
+        return len(Journals.objects.filter(journal_city=city_id))
+
+    # def get_absolute_url(self):
+    #     return reverse('journal', kwargs={'journal_city': self.journal_city})
 
 
 class JournalPhoto(models.Model):
     j_photo_journal = models.ForeignKey('Journals', on_delete=models.CASCADE, null=False)
     j_photo_image = models.ImageField(null=False, blank=False)
-    j_photo_number = models.IntegerField(auto_created=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+
+    # def __str__(self):
+    #     return self.j_photo_image
+
--- photo_booth/photos/views.py
+++ photo_booth/photos/views.py
@@ -1,39 +1,66 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
-from rest_framework.viewsets import ModelViewSet
 
-from photos.models import Photo, Cities, JournalPhoto, Journals
-from photos.serializers import PhotoSerializer
+from photos.models import Cities, JournalPhoto, Journals
 
 
-def photo_page(request):
-    return render(request, 'index.html', {'photos': Photo.objects.all()})
-
-
-class PhotoView(ModelViewSet):
-    queryset = Photo.objects.all()
-    serializer_class = PhotoSerializer
-
-
-def photos_app(request):
-    return render(request, 'main_app.html')
-
-
-def login(request):
-    return render(request, 'login.html')
+def main_page(request):
+    if request.user.is_authenticated:
+        return cities_selection(request)
+    return redirect('/login')
 
 
 def cities_selection(request):
-    return render(request, 'cities.html', {'cities_set': Cities.objects.all()})
+    available_cities = Cities.objects.filter(operators=request.user.id)
+    context = {
+        'cities_set': available_cities,
+    }
+    if len(available_cities) == 0:
+        context.update({'message': 'You have no cities, my friend.'})
+    if len(available_cities) == 1:
+        return get_journals(request, available_cities[0].id)
+    return render(request, 'cities.html', context)
 
 
-def journal(request):
+def get_journals(request, city_id):
+    city = Cities.objects.get(id=city_id)
+    user = User.objects.get(id=request.user.pk)
+    journals = Journals.objects.filter(journal_city=city).filter(journal_owner=user)
+    message = f"You have {len(journals)} journals."
+    last_journal = None
+
+    context = {
+        'message': message,
+        'current': f'Current city is {city}',
+        # 'j': len(Journals.objects.filter(journal_city=city_id)),
+        # 'u': Journals.objects.filter(journal_city=city_id),
+        # 'r': user,
+    }
+
+    if journals:
+        last_journal = journals.latest('time_create')
+        last_journal_photos = JournalPhoto.objects.filter(j_photo_journal=last_journal.id)
+        context['message'] += f" Current journal has {len(last_journal_photos)} photos in it."
+        if len(last_journal_photos) == 3:
+            last_journal = None
+
     if request.method == "POST":
         image = request.FILES.get('image')
-        photo = JournalPhoto.objects.create(
-            j_photo_journal=Journals.objects.all()[0],
+        context['success'] = 'You have successfully uploaded 1 photo'
+        if not last_journal:
+            last_journal = Journals.objects.create(
+                journal_city=city,
+                journal_owner_id=user.id,
+                time_create=datetime.now()
+            )
+        new_photo = JournalPhoto.objects.create(
+            j_photo_journal=last_journal,
             j_photo_image=image,
-            j_photo_number=1
+            time_create=datetime.now()
         )
-        redirect('cities.html')
 
-    return render(request, 'journal.html')
+        return redirect('journal', city_id)
+
+    return render(request, 'journal.html', context)
