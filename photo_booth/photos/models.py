efd4ec34c63b0cb8e55b32ad38c3877b739bcffb
av-kutergin <av.kutergin@gmail.com>
2022-08-20 23:55:29 +0400

    Added fields validation in admin, clean method, deleting files on object delete. Reworked view.get_journals logic.

--- photo_booth/photos/models.py
+++ photo_booth/photos/models.py
@@ -1,8 +1,8 @@
 import os
 import re
 
+from django.forms import ValidationError
 from django.contrib.auth.models import User
-from django.core.exceptions import SuspiciousOperation, FieldError
 from django.db import models
 from django.db.models.signals import post_save
 from django.urls import reverse
@@ -45,10 +45,13 @@ class Journal(models.Model):
     total_pages = models.IntegerField(null=True, verbose_name='Всего страниц', default=TOTAL_PAGES, editable=False)
     filled_pages = models.IntegerField(null=True, verbose_name='Заполнено страниц', default=0, editable=False)
 
+    def __str__(self):
+        return f'{self.journal_city.city_code}: {self.journal_name}'
+
     class Meta:
         verbose_name = 'Журнал'
         verbose_name_plural = 'Журналы'
-        ordering = ['journal_name']
+        ordering = ['journal_city', 'journal_name']
 
     def update_values(self):
         self.filled_pages = len(Photo.objects.filter(journal=self.id))
@@ -57,8 +60,32 @@ class Journal(models.Model):
     def get_absolute_url(self):
         return reverse('journal', kwargs={'journal_id': self.pk})
 
-    def __str__(self):
-        return f'{self.journal_city.city_code}: {self.journal_name}'
+    def clean(self):
+        if not self.journal_name:
+            return True
+        all_journals = Journal.objects.filter(journal_city=self.journal_city)
+        journals_in_city = set()
+
+        for journal in all_journals:
+            if journal != self:
+                journals_in_city.add(int(journal.journal_name.split('-')[-1]))
+
+        pattern = '(\d)+-(\d)+'
+        if not re.match(pattern, self.journal_name):
+            raise ValidationError({'journal_name': 'Название журнала должно состоять из двух чисел, разделённых дефисом.'})
+
+        first_page, last_page = map(int, self.journal_name.split('-'))
+        if (
+            ((first_page - 1) % TOTAL_PAGES != 0)
+            or (last_page % TOTAL_PAGES != 0)
+            or (last_page - first_page != TOTAL_PAGES - 1)
+        ):
+            raise ValidationError({'journal_name': 'Неверный интервал.'})
+
+        if last_page in journals_in_city:
+            raise ValidationError({'journal_name': 'Журнал с таким названием уже существует.'})
+
+        return True
 
 
 class Photo(models.Model):
@@ -74,22 +101,53 @@ class Photo(models.Model):
         verbose_name_plural = 'Фотографии'
         ordering = ['photo_name']
 
+    def __str__(self):
+        return f'{self.journal}, {self.photo_name}'
+
     def get_absolute_url(self):
         return reverse('furthermore',
                        kwargs={'city_code': self.journal.journal_city.city_code, 'journal_id': self.journal_id,
                                'photo_id': self.pk})
+
+    def clean(self):
+        if not self.photo_name:
+            return True
+        instance_journal = Journal.objects.get(pk=self.journal_id)
+        photos = Photo.objects.filter(journal=instance_journal)
+
+        photo_numbers_in_journal = set()
+        for photo in photos:
+            if photo != self and photo.photo_name:
+                photo_numbers_in_journal.add(int(photo.photo_name.split('.')[-1]))
+
+        first_page, last_page = map(int, instance_journal.journal_name.split('-'))
+
+        pattern = '((\d).)?(\d)+'
+        if not re.match(pattern, self.photo_name):
+            raise ValidationError({'photo_name': 'Название фотографии должно быть числом в формате: "<код города>.<номер фотографии>" или "<номер фотографии>".'})
+
+        photo_number = int(self.photo_name.split('.')[-1])
+        if (photo_number < first_page) or (photo_number > last_page):
+            raise ValidationError({'photo_name': "Номер фотографии должен входить в диапазон журнала."})
+
+        if photo_number in photo_numbers_in_journal:
+            raise ValidationError({'photo_name': "Фотография с таким номером уже существует."})
+
+        return True
 
 
 @receiver(post_save, sender=Photo)
 def update_photo_values(sender, instance, **kwargs):
     journal = Journal.objects.get(pk=instance.journal_id)
     photo_number, page_in_journal = get_photo_number(instance)
+
     if instance.photo_name:
         initial_path = instance.photo_image.path
         instance.photo_image = photo_path(instance, instance.photo_image.url.split('/')[-1])
         os.rename(initial_path, instance.photo_image.path)
-    instance.page_in_journal = page_in_journal
+
     instance.photo_name = f'{instance.journal.journal_city.city_code}.{photo_number}'
+    instance.page_in_journal = page_in_journal
     post_save.disconnect(update_photo_values, sender=Photo)
     instance.save()
     post_save.connect(update_photo_values, sender=Photo)
@@ -107,14 +165,7 @@ def get_photo_number(instance):
 
     first_page, last_page = map(int, instance_journal.journal_name.split('-'))
     if instance.photo_name:
-        pattern = '((\d).)?(\d)+'
-        if not re.match(pattern, instance.photo_name):
-            raise ValueError
         photo_number = int(instance.photo_name.split('.')[-1])
-        if (photo_number < first_page) or (photo_number > last_page):
-            raise FieldError
-        if photo_number in photo_numbers_in_journal:
-            raise SuspiciousOperation
         page_in_journal = photo_number - first_page + 1
         return photo_number, page_in_journal
 
@@ -129,18 +180,18 @@ def get_photo_number(instance):
 def update_journal(sender, instance, created, **kwargs):
     first_page, last_page = define_journal_pages(instance)
     instance.journal_name = f'{first_page}-{last_page}'
-
     photos = Photo.objects.filter(journal=instance)
     if not created:
         for photo in photos:
-            photo.photo_name = f'{instance.journal_city.city_code}.{first_page + photo.page_in_journal - 1}'
-            initial_path = photo.photo_image.path
-            photo.photo_image = photo_path(photo, photo.photo_image.url.split('/')[-1])
-            os.rename(initial_path, photo.photo_image.path)
+            if not (first_page <= int(photo.photo_name.split('.')[-1]) <= last_page):
+                photo.photo_name = f'{instance.journal_city.city_code}.{first_page + photo.page_in_journal - 1}'
+                initial_path = photo.photo_image.path
+                photo.photo_image = photo_path(photo, photo.photo_image.url.split('/')[-1])
+                os.rename(initial_path, photo.photo_image.path)
 
-            post_save.disconnect(update_photo_values, sender=Photo)
-            photo.save()
-            post_save.connect(update_photo_values, sender=Photo)
+                post_save.disconnect(update_photo_values, sender=Photo)
+                photo.save()
+                post_save.connect(update_photo_values, sender=Photo)
 
     post_save.disconnect(update_journal, sender=Journal)
     instance.save()
@@ -155,21 +206,7 @@ def define_journal_pages(instance):
             journals_in_city.add(int(journal.journal_name.split('-')[-1]))
 
     if instance.journal_name:
-        pattern = '(\d)+-(\d)+'
-        if not re.match(pattern, instance.journal_name):
-            raise ValueError
-
         first_page, last_page = map(int, instance.journal_name.split('-'))
-        if (
-            ((first_page - 1) % TOTAL_PAGES != 0)
-            or (last_page % TOTAL_PAGES != 0)
-            or (last_page - first_page != TOTAL_PAGES - 1)
-        ):
-            raise FieldError
-
-        if last_page in journals_in_city:
-            raise SuspiciousOperation
-
         return first_page, last_page
 
     if journals_in_city:
@@ -178,3 +215,10 @@ def define_journal_pages(instance):
         return first_page, last_page
 
     return 1, TOTAL_PAGES
+
+
+@receiver(models.signals.post_delete, sender=Photo)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    if instance.photo_image:
+        if os.path.isfile(instance.photo_image.path):
+            os.remove(instance.photo_image.path)
